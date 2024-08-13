176d10fe0cca05ce5e90ed0b730d0ab675500eae
Puuggordo <williamliu254@gmail.com>
2024-08-13 13:19:55 +1000

    Continued with quota

--- weather/weather_forecast.gd
+++ weather/weather_forecast.gd
@@ -4,12 +4,11 @@ extends Control
 @onready var day2 = $HBoxContainer/Tomorrow
 @onready var day3 = $HBoxContainer/AfterTomorrow
 @onready var day4 = $HBoxContainer/AfterAfterTomorrow
-@onready var quota = $HBoxContainer/Quota
-
+@onready var quota = $HBoxContainer2/Quota
 
 func _process(delta):
 	day1.text = Global.weather_forcast[0] + ", "
 	day2.text = Global.weather_forcast[1] + ", "
 	day3.text = Global.weather_forcast[2] + ", " 
 	day4.text = Global.weather_forcast[3]
-	quota.text
+	quota.text = "Quota: " + str(Global.quota) + " honey due in " + str(Global.days_until_quota) + " days"
